a62ab6fc75980b57d81bfd691bf334444094d150
shiroshan <siroshang@gmail.com>
2023-03-28 09:30:17 +0530

    feat: nav bar

---
+++ src/components/Atoms/Logo/Logo.component.tsx
@@ -0,0 +1,22 @@
+import Box from '@mui/material/Box';
+import React, { FC } from 'react';
+import Typography from '@mui/material/Typography';
+
+const Logo = () => {
+  return (
+    <Box px={4} py={1} textAlign='center'>
+      <a href="/" className='nav-link'>
+      <Typography
+        fontSize={40}
+        textAlign='center'
+        fontWeight={600}
+        textTransform='capitalize'
+      >
+        Modern Walk
+      </Typography>
+      </a>
+    </Box>
+  );
+};
+
+export default Logo;
---
+++ src/components/Organisms/NavBar/NavBar.component.tsx
@@ -0,0 +1,31 @@
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import React from 'react';
+import { Logo } from '../../Atoms/Logo';
+
+const NavBar = () => {
+  return (
+    <Box
+      width={1}
+      position='sticky'
+      bgcolor='#f5f5f5'
+      top={0}
+      zIndex={100}
+      borderColor='#D9D9D9'
+      borderBottom={2}
+    >
+      <Stack
+        direction='row'
+        alignItems='center'
+        justifyContent='center'
+        width={1}
+        mx='auto'
+        maxWidth={1100}
+      >
+        <Logo />
+      </Stack>
+    </Box>
+  );
+};
+
+export default NavBar;
