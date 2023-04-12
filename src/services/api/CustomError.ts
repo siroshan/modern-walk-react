7ba0394060fa0d745df007bbf67dccab0ab0a56f
shiroshan <siroshang@gmail.com>
2023-04-12 13:45:29 +0530

    refactor: Change error messages using enum

--- src/services/api/CustomError.ts
+++ src/services/api/CustomError.ts
@@ -1,5 +1,13 @@
 import { AxiosError } from 'axios';
 
+enum ErrorMessages{
+  badRequest='Invalid Request! pleach check the inputs.',
+  unauthorized= 'Your session has expired. Please sign in again!',
+  notFound='Requested resource not found!',
+  serverError='Sorry something went wrong from our side, please try again.',
+  default='Sorry something went wrong from our side, please try again.'
+}
+
 export class CustomError extends Error {
   constructor(message: string) {
     super(message);
@@ -11,34 +19,34 @@ export class CustomError extends Error {
 
     if (!err.isAxiosError) {
       return new CustomError(
-        'Sorry something went wrong from our side, please try again'
+        ErrorMessages.default
       );
     }
     if (err.code === 'ERR_NETWORK') {
       return new CustomError(
-        'Sorry something went wrong from our side, please try again'
+        ErrorMessages.default
       );
     }
     if (err.response) {
       switch (err.response.status) {
         case 400:
-          return new CustomError('Bad Request');
+          return new CustomError(ErrorMessages.badRequest);
 
         case 401:
           return new CustomError(
-            'Your session has expired. Please sign in again!'
+            ErrorMessages.unauthorized
           );
 
         case 404:
-          return new CustomError('Requested resource not found!');
+          return new CustomError(ErrorMessages.notFound);
 
         case 500:
           return new CustomError(
-            'Sorry something went wrong from our side, please try again'
+            ErrorMessages.serverError
           );
         default:
           return new CustomError(
-            'Sorry something went wrong from our side, please try again'
+            ErrorMessages.default
           );
       }
     }
