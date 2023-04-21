a00e0ab466ee2e3d49adb4f1371bdb0f9a286838
shiroshan <siroshang@gmail.com>
2023-04-21 12:17:06 +0530

    style: Replace productcard style with tailwind css

--- src/components/Molucules/ProductCard/ProductCard.component.tsx
+++ src/components/Molucules/ProductCard/ProductCard.component.tsx
@@ -5,54 +5,45 @@ import { FC } from 'react';
 import { IProduct } from '../../../models/Product';
 
 const ProductCard: FC<{ product: IProduct }> = ({ product }) => {
-  const getBgColor = () => {
-    if (product.category === "men's clothing") {
-      return '#2BD9AF';
-    } else if (product.category === "women's clothing") {
-      return '#FF5E84';
-    }
-    return 'coral';
-  };
+  const bgColor = product.category === "men's clothing" ? '#2BD9AF' : '#FF5E84';
+
   return (
-    <div vocab='https://schema.org/' typeof='Product' className='product-card'>
-      <Box p={2}>
-        <Typography
-          textAlign='center'
-          textTransform='capitalize'
+    <div
+      vocab='https://schema.org/'
+      typeof='Product'
+      className='product-card relative mx-auto h-96 w-full rounded-3xl bg-white shadow'
+    >
+      <div className='p-2'>
+        <div
+          className='h-20 text-center font-medium capitalize'
           property='name'
-          height={72}
-          fontWeight={500}
         >
           {product.title}
-        </Typography>
-        <div className='img-wrap'>
-          <img src={product.image} alt='product image' property='image' />
         </div>
-      </Box>
-      <Box
-        bgcolor={getBgColor}
-        borderRadius='50px'
-        p={2}
-        position='absolute'
-        bottom={0}
-        width={1}
-        boxSizing='border-box'
+        <div className='w-full'>
+          <img
+            className='mx-auto h-auto max-h-32 w-full object-contain'
+            src={product.image}
+            alt='product'
+            property='image'
+          />
+        </div>
+      </div>
+      <div
+        className={`absolute bottom-0 box-border w-full rounded-3xl p-2 bg-[${bgColor}]`}
       >
-        <Stack direction='column' alignItems='center' spacing={1}>
-          <Typography property='price' color='blue'>
+        <div className='flex flex-col items-center'>
+          <div property='price' className='text-blue-700'>
             Rs {(Math.round(product.price * 100) / 100).toFixed(2)}
-          </Typography>
-          <Typography
+          </div>
+          <div
             property='description'
-            textAlign='center'
-            className='prod-desc'
-            fontSize={14}
-            maxWidth={200}
+            className='line-clamp-4 max-w-[200px] text-ellipsis text-center text-sm'
           >
             {product.description}
-          </Typography>
-        </Stack>
-      </Box>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
