cdc4401c85b838a4064a712d183da3b083465647
shiroshan <siroshang@gmail.com>
2023-05-01 10:28:37 +0530

    style: Add padding to layout

--- src/components/Layouts/MaxWidthLayout/MaxWidthLayout.component.tsx
+++ src/components/Layouts/MaxWidthLayout/MaxWidthLayout.component.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 
 const MaxWidthLayout: FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className='mx-auto w-full max-w-screen-lg'>{children}</div>;
+  return <div className='mx-auto w-full max-w-screen-lg px-5'>{children}</div>;
 };
 
 export default MaxWidthLayout;
