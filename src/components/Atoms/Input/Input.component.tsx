bb850efab70fde2afefd30ea1e2288f7fc0d7180
shiroshan <siroshang@gmail.com>
2023-05-04 11:40:34 +0530

    style: Add error state design

--- src/components/Atoms/Input/Input.component.tsx
+++ src/components/Atoms/Input/Input.component.tsx
@@ -3,17 +3,26 @@ import { cn } from '../../../lib/utils';
 import { InputProps } from './Input.type';
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, error, helperText, ...props }, ref) => {
     return (
-      <input
-        type={type}
-        className={cn(
-          'flex h-10 w-full rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50',
-          className
+      <>
+        <input
+          type={type}
+          className={cn(
+            `${
+              error ? 'border-danger' : 'border-input'
+            } flex h-10 w-full rounded-md border bg-transparent px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50`,
+            className
+          )}
+          ref={ref}
+          {...props}
+        />
+        {error && (
+          <span className='text-left text-sm font-normal text-danger'>
+            {helperText}
+          </span>
         )}
-        ref={ref}
-        {...props}
-      />
+      </>
     );
   }
 );
